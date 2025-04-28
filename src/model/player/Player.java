10e9c35a838860204b09b312df4ed53e79c7a31c
Duc Cuong <cuongmerlin@gmail.com>
2025-04-28 16:38:07 +0700

    change

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -5,15 +5,15 @@ import model.core.card.Card;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
-    protected List<Card> hand;
-    protected List<Card> selectedCards;
-    protected String name;
+public abstract class Player {
+    private List<Card> hand;
+    private List<Card> selectedCards;
+    private String name;
 
     public Player() {
         this.hand = new ArrayList<>(); // Khởi tạo hand
         this.selectedCards = new ArrayList<>(); // Khởi tạo selectedCards
-        this.name = "Unnamed Player";
+        this.name = "Anonymous Player";
     }
 
     public Player(String name) {
@@ -45,6 +45,22 @@ public class Player {
     public List<Card> playCard() {
         hand.removeAll(selectedCards);
         return new ArrayList<>(this.selectedCards);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
+
+    public void setSelectedCards(List<Card> selectedCards) {
+        this.selectedCards = selectedCards;
     }
 
 
