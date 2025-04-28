bfa3797aec419951d4d95727d36104f3ac4c9a7f
cuongdztop1tg <caocuong090905@gmail.com>
2025-04-28 16:53:10 +0700

    Changes to core player

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -5,9 +5,9 @@ import model.core.card.Card;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
-    protected List<Card> hand;
-    protected List<Card> selectedCards;
+public class Player<T extends Card> {
+    protected List<T> hand;
+    protected List<T> selectedCards;
     protected String name;
 
     public Player() {
@@ -22,7 +22,7 @@ public class Player {
         this.name = name;
     }
 
-    public void receiveCard(Card card) {
+    public void receiveCard(T card) {
         hand.add(card);
     }
 
@@ -30,23 +30,20 @@ public class Player {
         return hand.size();
     }
 
-    public List<Card> getHand() {
+    public List<T> getHand() {
         return new ArrayList<>(this.hand);
     }
 
-    public List<Card> getSelectedCards() {
+    public List<T> getSelectedCards() {
         return new ArrayList<>(this.selectedCards);
     }
 
-    public void selectCard(Card card) {
+    public void selectCard(T card) {
         selectedCards.add(card);
     }
 
-    public List<Card> playCard() {
+    public List<T> playCard() {
         hand.removeAll(selectedCards);
         return new ArrayList<>(this.selectedCards);
     }
-
-
-
 }
